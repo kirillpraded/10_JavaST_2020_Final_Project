3189c2b6980580b82536bfcbfb889ab38d6cd51d
Kiryl Praded <kirillpraded@bk.ru>
2021-02-01 18:13:32 +0300

    Update 2_create_database.sql

--- AskAndGo/sql/2_create_database.sql
+++ AskAndGo/sql/2_create_database.sql
@@ -3,16 +3,7 @@ CREATE DATABASE `askandgo_db` DEFAULT CHARACTER SET utf8;
 CREATE USER 'person'@'localhost' IDENTIFIED BY 'kirillpraded';
 GRANT SELECT,INSERT,UPDATE,DELETE
     ON askandgo_db.*
-    TO 'CREATE USER ''person''@''localhost'' IDENTIFIED BY ''kirillpraded'';
-GRANT SELECT,INSERT,UPDATE,DELETE
-ON askandgo_db.*
-TO ''person''@''localhost'';
-
-GRANT SELECT,INSERT,UPDATE,DELETE
-ON askandgo_db.*
-TO ''person''@''localhost'';
-
-FLUSH PRIVILEGES;'@'localhost';
+    TO 'person'@'localhost';
 
 GRANT SELECT,INSERT,UPDATE,DELETE
     ON askandgo_db.*
